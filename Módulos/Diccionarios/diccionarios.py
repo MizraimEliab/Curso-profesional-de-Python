261c9ab91e0833943bd1c8920931a9ce6b07763f
Mizraim Eliab <mizraimeliab168@gmail.com>
2024-02-29 08:41:42 -0600

    diccionarios

---
+++ Módulos/Diccionarios/diccionarios.py
@@ -0,0 +1,20 @@
+elementos = {}
+
+# Crear la llave y su valor
+elementos['nombre'] = 'Alan'
+elementos[(1,2,3)] = 'La llave es una tupla'
+
+# Actualiza el valor de la llave
+elementos['nombre'] = 'Dante'
+
+
+print(elementos)
+
+print(len(elementos))
+
+
+elementos = {'a': 1, 'b': 2, 'c': 3, 'a':4}
+print(elementos)
+
+print(elementos['a'])
+
