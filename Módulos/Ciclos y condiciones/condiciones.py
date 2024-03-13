d0b9b05f1c622d8b7ec8a302773360a962113da7
Mizraim Eliab <mizraimeliab168@gmail.com>
2024-03-13 08:23:09 -0600

    add conditions class

---
+++ Módulos/Ciclos y condiciones/condiciones.py
@@ -0,0 +1,10 @@
+resultado = 10
+
+if resultado > 10:
+    print('La variable cumple con la condición.')
+
+
+if resultado > 10 and resultado < 20:
+    print('La variable cumple con la condición.')
+else:
+    print('La variable no cumple con la condición.')
