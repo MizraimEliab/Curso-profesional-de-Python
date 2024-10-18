43dde545c838883c22f94a87f7732c253657bddc
Mizraim Eliab <mizraimeliab168@gmail.com>
2024-10-17 22:56:09 -0600

    add test class

--- Módulos/Funciones/documentar.py
+++ Módulos/Funciones/documentar.py
@@ -10,8 +10,29 @@ def suma(numero_uno, numero_dos):
     numero_dos (int)
 
     Se retorna la suma de los parámetros.
+
     """
     return numero_uno + numero_dos
 
 print(suma.__doc__)
 print(help(suma))
+
+# Testear funciones
+
+def suma(numero_uno, numero_dos):
+    """
+    La función suma dos números enteros.
+
+    Argumentos:
+    numero_uno (int)
+    numero_dos (int)
+
+    Se retorna la suma de los parámetros.
+
+    >>> suma(10, 15)
+    25
+
+    >>> suma(25, 25)
+    50
+    """
+    return numero_uno + numero_dos
